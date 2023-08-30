8e1f1d0f83cfcb29691597cf3e79509c0be1ec42
Andy Ficek <aficek@precisionplanting.com>
2023-08-30 16:59:23 -0500

    built options pop up

--- public/js/notes.js
+++ public/js/notes.js
@@ -4,6 +4,8 @@ for (let i = 0; i < tag.length; i++) {
   if (tag[i].innerHTML.toUpperCase() == "PERSONAL") {
     tag[i].style.background = "#FB7777";
   } else if (tag[i].innerHTML.toUpperCase() == "PROFESSIONAL") {
-    tag[i].style.background = "blue";
+    tag[i].style.background = "#2C9A4B";
+  } else if (tag[i].innerHTML.toUpperCase() == "ALL") {
+    tag[i].style.background = "#B977FB";
   }
 }
