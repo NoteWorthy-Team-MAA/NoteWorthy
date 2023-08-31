f597b94f6ee95234e745eccd1b1a953a40693d6c
Andy Ficek <aficek@precisionplanting.com>
2023-08-31 14:57:25 -0500

    added modal for category

--- public/js/notes.js
+++ public/js/notes.js
@@ -1,11 +1,22 @@
-let tag = document.querySelectorAll(".tag");
+document.addEventListener("DOMContentLoaded", () => {
+  let tag = document.querySelectorAll(".tag");
 
-for (let i = 0; i < tag.length; i++) {
-  if (tag[i].innerHTML.toUpperCase() == "PERSONAL") {
-    tag[i].style.background = "#FB7777";
-  } else if (tag[i].innerHTML.toUpperCase() == "PROFESSIONAL") {
-    tag[i].style.background = "#2C9A4B";
-  } else if (tag[i].innerHTML.toUpperCase() == "ALL") {
-    tag[i].style.background = "#B977FB";
+  for (let i = 0; i < tag.length; i++) {
+    if (tag[i].innerHTML.toUpperCase() == "PERSONAL") {
+      tag[i].style.background = "#FB7777";
+    } else if (tag[i].innerHTML.toUpperCase() == "PROFESSIONAL") {
+      tag[i].style.background = "#2C9A4B";
+    } else if (tag[i].innerHTML.toUpperCase() == "ALL") {
+      tag[i].style.background = "#B977FB";
+    }
   }
-}
+
+  document.querySelector("#filterByDateText").addEventListener("click", (e) => {
+    if (e.target.innerText == "Sort By Date (Descending)") {
+      e.target.innerText = "Sort By Date (Ascending)";
+    } else {
+      e.target.innerText = "Sort By Date (Descending)";
+    }
+    document.querySelector("#filterByDate").classList.toggle("rotated-icon");
+  });
+});
