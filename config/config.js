db26efb949b29a34dfc69d6db4ef74bcc6cc2cae
Andre Ellis Jr <andre.ellis@precisionplanting.com>
2023-09-06 11:10:29 -0500

    correcting the connections with a dotenv file

---
+++ config/config.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: "postgres",
+    port: process.env.PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: "postgres",
+    port: process.env.PORT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    },
+  },
+};
