b1223213ff011603c1083d74af3b279be92f7539
mirandamorton1 <miranda.morton1@gmail.com>
2023-09-06 10:11:55 -0500

    styling cleaned up, button working on mobile view as well for recording

--- public/js/speech-modal.js
+++ public/js/speech-modal.js
@@ -1,52 +1,48 @@
-const recordBtn = document.querySelector(".record")
-const addBtn = document.querySelector(".add")
-const result = document.querySelector(".result")
-const interim = document.querySelector(".interim")
-const clearBtn = document.querySelector(".clear");
-const body = document.getElementById("body")
+const recordBtn = document.querySelectorAll(".record");
+const result = document.querySelector(".result");
+const interim = document.querySelector(".interim");
 
 let SpeechRecognition =
     window.SpeechRecognition || window.webkitSpeechRecognition,
   recognition,
   recording = false;
 
-
-function speechToText() {
+function speechToText(i) {
   try {
     recognition = new SpeechRecognition();
     recognition.interimResults = true;
-    recordBtn.classList.add("recording");
-    recordBtn.querySelector('img').setAttribute("src", "../public/img/recordingicon.svg" )
+    i.classList.add("recording");
+    i
+      .querySelector("img")
+      .setAttribute("src", "../public/img/recordingicon.svg");
     recognition.start();
     recognition.onresult = (event) => {
       const speechResult = event.results[0][0].transcript;
-      //detect when intrim results
+
       if (event.results[0].isFinal) {
         result.innerHTML += " " + speechResult;
-
-      // const contentToInsert = result.innerHTML
-      // console.log(result.innerHTML)
-      tinyMCE.execCommand('mceInsertContent',false,speechResult + " ");
-      tinyMCE.activeEditor.selection.select(tinyMCE.activeEditor.getBody(), true);
-      tinyMCE.activeEditor.selection.collapse(false);
-
+        tinyMCE.execCommand("mceInsertContent", false, speechResult + " ");
+        tinyMCE.activeEditor.selection.select(
+          tinyMCE.activeEditor.getBody(),
+          true
+        );
+        tinyMCE.activeEditor.selection.collapse(false);
       } else {
-        //creative p with class interim if not already there
+
         if (!document.querySelector(".interim")) {
           const interim = document.createElement("p");
           interim.classList.add("interim");
           result.appendChild(interim);
         }
-        //update the interim p with the speech result
+
         document.querySelector(".interim").innerHTML = " " + speechResult;
       }
-
     };
     recognition.onspeechend = () => {
       speechToText();
     };
     recognition.onerror = (event) => {
-        console.log(event)
+      console.log(event);
       stopRecording();
       if (event.error === "no-speech") {
         alert("No speech was detected. Stopping...");
@@ -69,25 +65,24 @@ function speechToText() {
   }
 }
 
-recordBtn.addEventListener("click", () => {
-  if (!recording) {
-    speechToText();
-    recording = true;
-  } else {
-    stopRecording();
-  }
-});
+for (let i of recordBtn) {
+  i.addEventListener("click", (e) => {
+    if (!recording) {
+      speechToText(i);
+      recording = true;
+    } else {
+      stopRecording(i);
+    }
+  });
+}
 
-function stopRecording() {
+function stopRecording(i) {
   recognition.stop();
-  recordBtn.querySelector('img').setAttribute("src", "../public/img/micIcon.svg")
-  recordBtn.classList.remove("recording");
+  i
+    .querySelector("img")
+    .setAttribute("src", "../public/img/micIcon.svg");
+  i.classList.remove("recording");
   recording = false;
   result.innerHTML = "";
   interim.innerHTML = "";
 }
-
-clearBtn.addEventListener("click", () => {
-  result.innerHTML = "";
-
-});
